92ab0b3e998a0f944dba89887b6efa1ad1864efa
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2018-11-21 01:27:24 +0700

    Queue Worker

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,10 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('check-queue', function () {
+    Mail::to('tinnakornchoompee@gmail.com')->send(new Testmail());
+    return "Working";
 });
